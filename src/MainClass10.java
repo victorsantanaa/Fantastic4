f92497a3ca0dcef0f5338f4986c08e289c18631c
victorsantanaa <victor.santanaribeiro@gmail.com>
2021-08-13 23:46:30 -0300

    Implementação pós-video

--- src/MainClass10.java
+++ src/MainClass10.java
@@ -1,15 +1,23 @@
 import java.util.Scanner;
-public class MainClass10{ 
-  public static void main(String args[]){
-       Scanner _key = new Scanner(System.in);
-double  a;
-double  d;
-String  t1;
-String  t2;
-t1 = 10;
-d = "abc";
-System.out.println(t1);
-a = 10;
-a=Double.parseDouble(_key.nextLine());
-a = 1+2*3;
-  }}
+
+public class MainClass10 {
+    public static void main(String args[]) {
+        Scanner _key = new Scanner(System.in);
+        double a;
+        double d;
+        String t1;
+        String t2;
+        a = 10;
+        d = 11.8;
+        t1 = "abc";
+        System.out.println(t1);
+        a = Double.parseDouble(_key.nextLine());
+        d = Double.parseDouble(_key.nextLine());
+        if (a > d) {
+            System.out.println(a);
+        } else {
+            System.out.println(d);
+        }
+
+    }
+}
